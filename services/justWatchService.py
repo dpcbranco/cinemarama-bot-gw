fc9fad9caad96b6347a40dc7fadfda88491e2e45
Daniel P. C. Branco <danielpcbranco@gmail.com>
2023-03-18 00:04:01 -0300

    Refatora montagem de chave doa busca de filme

--- services/justWatchService.py
+++ services/justWatchService.py
@@ -4,5 +4,9 @@ just_watch = JustWatch(country='BR')
 
 def search(query):
     response = just_watch.search_for_item(query=query)
-    movieList = {movie["jw_entity_id"]:"{} ({})".format(movie["title"], movie["original_release_year"]) for movie in response["items"][:10]}
+    movieList = {
+        movie["jw_entity_id"]:"{} ({})"
+            .format(movie["title"], movie["original_release_year"])
+        for movie in response["items"][:10]
+    }
     return movieList
